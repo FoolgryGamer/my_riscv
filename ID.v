5da041957a582bf4d7e55a6965d0d79d78fd4ec9
FoolgryGamer <40832757+FoolgryGamer@users.noreply.github.com>
2023-09-14 23:52:41 +0800

    update

--- ID.v
+++ ID.v
@@ -1,68 +1,69 @@
-`include "const.v"
-`include "control.v"
-`include "register.v"
-`include "imm_gen.v"
 
-module ID (
-  input  wire                         clk,
-  input  wire                         rst,
-  input  wire [    `INST_WIDTH - 1:0] inst,             // Input inst from IF/ID
-  input  wire                         RegWrite_in,      // read from MEM/WB
-  input  wire [`REG_DATA_WIDTH - 1:0] write_data,       // write_back data from WB
-  input  wire [  `REG_ADDR_WIDTH-1:0] rd,               // read from WB
-  input  wire [  `REG_ADDR_WIDTH-1:0] rs1,              // read from IF/ID
-  input  wire [  `REG_ADDR_WIDTH-1:0] rs2,              // read from IF/ID
-  input  wire [  `OPCODE_WIDTH - 1:0] opcode,           // read from IF/ID
-  input  wire                         stall,            // read from HAZARD_DETECTION
-  // ---- Control Signals ---- ALL output to ID/EX
-  output wire                         Branch,
-  output wire                         MemRead,
-  output wire                         MemtoReg,
-  output wire                         MemWrite,
-  output wire                         ALUSrc,
-  output wire                         RegWrite,
-  // -------------------------
-  output wire [`REG_DATA_WIDTH - 1:0] read_reg_data_1,  // read data from Register
-  output wire [`REG_DATA_WIDTH - 1:0] read_reg_data_2,  // read data from Register
-  output wire [`REG_DATA_WIDTH - 1:0] imm               // imm, output to ID/EX
-  // output wire IF_flush                                 // output to IF/ID
+module core_id_stage(
+    input  logic [31:0]  i_instr,
+    output logic         o_src1_reg_en, o_src2_reg_en,
+    output logic         o_jal, o_alures2reg, o_memory2reg, o_mem_write,
+    output logic [ 4:0]  o_src1_reg_addr, o_src2_reg_addr, o_dst_reg_addr,
+    output logic [ 6:0]  o_opcode, o_funct7,
+    output logic [ 2:0]  o_funct3,
+    output logic [31:0]  o_imm
 );
 
-  control u_control (
-    .inst    (inst),
-    .opcode  (opcode),
-    .Branch  (Branch),
-    .MemRead (MemRead),
-    .MemtoReg(MemtoReg),
-    .MemWrite(MemWrite),
-    .ALUSrc  (ALUSrc),
-    .RegWrite(RegWrite),
-    .stall   (stall)
-  );
+assign {o_funct7, o_src2_reg_addr, o_src1_reg_addr, o_funct3, o_dst_reg_addr, o_opcode} = i_instr;
 
-  register u_register (
-    .clk            (clk),
-    .reset          (rst),
-    .RegWrite       (RegWrite_in),
-    .read_reg_addr_1(rs1),
-    .read_reg_addr_2(rs2),
-    .write_reg_addr (rd),
-    .write_reg_data (write_data),
-    .read_reg_data_1(read_reg_data_1),
-    .read_reg_data_2(read_reg_data_2)
-  );
+enum {UKNOWN_TYPE, R_TYPE, I_TYPE, IZ_TYPE, S_TYPE, B_TYPE, U_TYPE, J_TYPE} instr_type;
 
-  imm_gen u_imm_gen (
-    .inst(inst),
-    .imm (imm)
-  );
+localparam  OPCODE_AUIPC         = 7'b0010111,   // rd=pc+imm
+            OPCODE_LUI           = 7'b0110111,   // rd=imm;
+            OPCODE_JAL           = 7'b1101111,   // rd=pc+4,            pc= pc+imm*2,
+            OPCODE_JALR          = 7'b1100111,   // rd=pc+4,            pc= rs1+imm
+            OPCODE_BRANCH        = 7'b1100011,   // conditional branch, pc= pc+imm*2,
+            OPCODE_ALI           = 7'b0010011,   // arithmetic and logical I-TYPE, rd=alu_res
+            OPCODE_ALR           = 7'b0110011,   // arithmetic and logical R-TYPE, rd=alu_res
+            OPCODE_LOAD          = 7'b0000011,   // load
+            OPCODE_STORE         = 7'b0100011;   // store, rd=loadvalue
 
-  // CONTROL_HAZARD u_CONTROL_HAZARD (
-  //                    .reg_data_1(read_reg_data_1),
-  //                    .reg_data_2(read_reg_data_2),
-  //                    .inst(inst),
-  //                    .IF_flush(IF_flush)
-  //                );
+// generate control signals
+assign o_jal             = (o_opcode == OPCODE_JAL );
+assign o_memory2reg      = (o_opcode == OPCODE_LOAD );
+assign o_mem_write       = (o_opcode == OPCODE_STORE);
+assign o_alures2reg      = (o_opcode == OPCODE_JAL || o_opcode == OPCODE_JALR  ||
+                            o_opcode == OPCODE_LUI || o_opcode == OPCODE_AUIPC || 
+                            o_opcode == OPCODE_ALI || o_opcode == OPCODE_ALR     );
 
+always_comb         // calculate instruction type
+    case(o_opcode)
+        OPCODE_AUIPC  : instr_type <= U_TYPE;
+        OPCODE_JAL    : instr_type <= J_TYPE;
+        OPCODE_JALR   : instr_type <= I_TYPE;
+        OPCODE_BRANCH : instr_type <= B_TYPE;
+        OPCODE_LUI    : instr_type <= U_TYPE;
+        OPCODE_ALI    : instr_type <= I_TYPE;
+        OPCODE_ALR    : instr_type <= R_TYPE;
+        OPCODE_LOAD   : instr_type <= I_TYPE;
+        OPCODE_STORE  : instr_type <= S_TYPE;
+        default       : instr_type <= UKNOWN_TYPE;
+    endcase
+    
+always_comb         // calculate imm
+    case(instr_type)
+        I_TYPE : o_imm <= {{20{i_instr[31]}} , i_instr[31:20]};
+        IZ_TYPE: o_imm <= { 20'h0          , i_instr[31:20]};
+        S_TYPE : o_imm <= {{20{i_instr[31]}} , i_instr[31:25], i_instr[11:7]};
+        B_TYPE : o_imm <= {{20{i_instr[31]}} , i_instr[7], i_instr[30:25], i_instr[11:8], 1'b0};
+        U_TYPE : o_imm <= { i_instr[31:12]   , 12'h0 };
+        J_TYPE : o_imm <= {{12{i_instr[31]}} , i_instr[19:12], i_instr[20], i_instr[30:21], 1'b0};
+        default: o_imm <= 0;
+    endcase
+    
+always_comb         // calculate read register enable
+    case(instr_type)
+        R_TYPE : {o_src2_reg_en, o_src1_reg_en} <= 2'b11;
+        I_TYPE : {o_src2_reg_en, o_src1_reg_en} <= 2'b01;
+        IZ_TYPE: {o_src2_reg_en, o_src1_reg_en} <= 2'b01;
+        S_TYPE : {o_src2_reg_en, o_src1_reg_en} <= 2'b11;
+        B_TYPE : {o_src2_reg_en, o_src1_reg_en} <= 2'b11;
+        default: {o_src2_reg_en, o_src1_reg_en} <= 2'b00;
+    endcase
 
 endmodule
--- alu.v
+++ alu.v
@@ -1,46 +1,87 @@
-`include "const.v"
 
-module ALU (
-  input wire [`ALU_DATA_WIDTH - 1:0] input_data_1,
-  input wire [`ALU_DATA_WIDTH - 1:0] input_data_2,
-  input wire [`ALU_CONTROL_WIDTH - 1:0] ALU_control,
-  input wire [`INST_ADDR_WIDTH - 1:0] pc,
-  output wire zero,
-  output reg [`ALU_DATA_WIDTH - 1:0] output_data
+module core_alu(
+    input   [ 6:0] i_opcode, i_funct7,
+    input   [ 2:0] i_funct3,
+    input   [31:0] i_num1u, i_num2u, i_pc, i_immu,
+    output  reg        o_branch_jalr,
+    output  reg  [31:0] o_res, o_branch_jalr_target
 );
 
-  wire data_1_sign;
-  wire data_2_sign;
+wire [ 4:0] shamt_rs, shamt_imm;
+wire [31:0] num1_plus_imm, pc_plus_imm;
+wire signed [31:0] i_num1s, i_num2s, i_imms;
 
-  assign data_1_sign = input_data_1[`ALU_DATA_WIDTH-1];
-  assign data_2_sign = input_data_2[`ALU_DATA_WIDTH-1];
+assign shamt_imm     = i_immu[4:0];
+assign shamt_rs      = i_num2u[4:0];
+assign num1_plus_imm = i_num1u + i_immu;
+assign pc_plus_imm   = i_pc    + i_immu;
+assign i_num1s       = i_num1u;
+assign i_num2s       = i_num2u;
+assign i_imms        = i_immu;
 
-  always @(*) begin
-    case (ALU_control)
-      `ALU_AND: output_data = input_data_1 & input_data_2;
-      `ALU_OR:  output_data = input_data_1 | input_data_2;
-      `ALU_ADD: output_data = input_data_1 + input_data_2;
-      `ALU_SUB: output_data = input_data_1 - input_data_2;
-      `ALU_NOR: output_data = ~(input_data_1 | input_data_2);
-      `ALU_LT: begin
-        case ({
-          data_1_sign, data_2_sign
-        })
-          2'b00, 2'b11:  // With same sign
-          output_data = input_data_1 < input_data_2 ? 0 : 1;
-          2'b10:  // data_1 < 0 , data_2 >= 0
-          output_data = 0;
-          2'b01:  // data_1 >= 0 , data_2 < 0
-          output_data = 1;
-        endcase
-      end
-      `ALU_XOR: output_data = input_data_1 ^ input_data_2;
-      `ALU_SLL: output_data = input_data_1 << input_data_2;
-      `ALU_SRL: output_data = input_data_1 >> input_data_2;
-      `ALU_JAL: output_data = pc + 4;
-      default:  ;
+always(*)
+    case(i_opcode)
+        7'b1100111 : begin                       // JALR
+                         o_branch_jalr <= 1'b1;
+                         o_branch_jalr_target <= num1_plus_imm;
+                     end
+        7'b1100011 : begin                       // BRANCH绫?
+                         case(i_funct3)
+                             3'b000 : o_branch_jalr <= (i_num1u == i_num2u);   // BEQ
+                             3'b001 : o_branch_jalr <= (i_num1u != i_num2u);   // BNE
+                             3'b100 : o_branch_jalr <= (i_num1s <  i_num2s);   // BLT
+                             3'b101 : o_branch_jalr <= (i_num1s >= i_num2s);   // BGE
+                             3'b110 : o_branch_jalr <= (i_num1u <  i_num2u);   // BLTU
+                             3'b111 : o_branch_jalr <= (i_num1u >= i_num2u);   // BGEU
+                             default: o_branch_jalr <= 1'b0;
+                         endcase
+                         o_branch_jalr_target <= pc_plus_imm;
+                     end
+        default    : begin                       // 涓嶈烦杞?
+                         o_branch_jalr <= 1'b0;
+                         o_branch_jalr_target <= 0;
+                     end
     endcase
-  end
 
-  assign zero = (ALU_control == `ALU_JAL) ? 1'b1 : (output_data == 0 ? 1'b1 : 1'b0);
+always(*)
+    casex({i_funct7,i_funct3,i_opcode})
+        // JAL, JALR
+        17'bxxxxxxx_xxx_110x111 : o_res <=  i_pc + 4;             // JAL, JALR
+        // LUI
+        17'bxxxxxxx_xxx_0110111 : o_res <=  i_immu;               // LUI
+        // AUIPC
+        17'bxxxxxxx_xxx_0010111 : o_res <=  pc_plus_imm       ;   // AUIPC
+        // 算术类
+        17'b0000000_000_0110011 : o_res <=  i_num1u +  i_num2u;   // ADD
+        17'bxxxxxxx_000_0010011 : o_res <=  num1_plus_imm     ;   // ADDI
+        17'b0100000_000_0110011 : o_res <=  i_num1u -  i_num2u;   // SUB
+        // 逻辑类
+        17'b0000000_100_0110011 : o_res <=  i_num1u ^  i_num2u;   // XOR
+        17'bxxxxxxx_100_0010011 : o_res <=  i_num1u ^  i_immu ;   // XORI
+        17'b0000000_110_0110011 : o_res <=  i_num1u |  i_num2u;   // OR
+        17'bxxxxxxx_110_0010011 : o_res <=  i_num1u |  i_immu ;   // ORI
+        17'b0000000_111_0110011 : o_res <=  i_num1u &  i_num2u;   // AND
+        17'bxxxxxxx_111_0010011 : o_res <=  i_num1u &  i_immu ;   // ANDI
+        // 移位类
+        17'b0000000_001_0110011 : o_res <=  i_num1u << shamt_rs ; // SLL
+        17'b0000000_001_0010011 : o_res <=  i_num1u << shamt_imm; // SLLI
+        17'b0000000_101_0110011 : o_res <=  i_num1u >> shamt_rs ; // SRL
+        17'b0000000_101_0010011 : o_res <=  i_num1u >> shamt_imm; // SRL
+        17'b0100000_101_0110011 : begin                           // SRA
+                                      o_res <= i_num1u >> shamt_rs;
+                                      for(int i=0;i< shamt_rs;i++) o_res[31-i] <= i_num1u[31];
+                                  end
+        17'b0100000_101_0010011 : begin                           // SRAI
+                                      o_res <= i_num1u >> shamt_imm;
+                                      for(int i=0;i<shamt_imm;i++) o_res[31-i] <= i_num1u[31];
+                                  end
+        // 条件SET类
+        17'b0000000_010_0110011 : o_res <=  (i_num1s < i_num2s) ? 1 : 0;   // SLT
+        17'bxxxxxxx_010_0010011 : o_res <=  (i_num1s < i_imms ) ? 1 : 0;   // SLTI
+        17'b0000000_011_0110011 : o_res <=  (i_num1u < i_num2u) ? 1 : 0;   // SLTU
+        17'bxxxxxxx_011_0010011 : o_res <=  (i_num1u < i_immu ) ? 1 : 0;   // SLTIU
+        // 未定义
+        default   : o_res <= 0;
+    endcase
+
 endmodule
